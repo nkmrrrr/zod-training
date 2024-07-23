59bdb1bf523eb54cc65a99471900ac5ed6cd9446
nkmr <ryusei1597@gmail.com>
2024-07-23 23:03:59 +0900

    basic

--- basic.ts
+++ basic.ts
@@ -5,3 +5,16 @@ const mySchema = z.string();
 mySchema.parse("hello"); // "hello"
 mySchema.parse(123); // Throws an error
 
+mySchema.safeParse("hello"); // { success: true, data: "hello" }
+mySchema.safeParse(123); // { success: false, error: [error] }
+
+console.log(mySchema.safeParse(123))
+
+const User = z.object({
+  username: z.string(),
+});
+
+User.parse({ username: "johndoe" }); // { username: "johndoe" }
+
+type User = z.infer<typeof User>;
+
