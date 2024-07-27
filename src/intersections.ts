f23fe59b8f6ea04c033c1040efdf76791718b8cf
nkmryu <ryusei1597@gmail.com>
2024-07-27 15:10:46 +0900

    intersections

---
+++ src/intersections.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+const Person = z.object({
+  name: z.string(),
+});
+
+const Employee = z.object({
+  role: z.string(),
+});
+
+const EmployedPerson = z.intersection(Person, Employee);
+
+const a = z.union([z.number(), z.string()]);
+const b = z.union([z.number(), z.boolean()]);
+const c = z.intersection(a, b);
+
+type c = z.infer<typeof c>; // => number
