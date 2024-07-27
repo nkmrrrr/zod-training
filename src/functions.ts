4ae9f12fba34e664662db309339a0261cb7b5450
nkmryu <ryusei1597@gmail.com>
2024-07-27 15:45:34 +0900

    functions

---
+++ src/functions.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const trimmedLength = z
+  .function()
+  .args(z.string()) // accepts an arbitrary number of arguments
+  .returns(z.number())
+  .implement((x) => {
+    // TypeScript knows x is a string!
+    return x.trim().length;
+  });
+
+trimmedLength("sandwich"); // => 8
+trimmedLength(" asdf "); // => 4
+
+const myFunction = z
+  .function()
+  .args(z.string())
+  .implement((arg) => {
+    return [arg.length];
+  });
+
+myFunction; // (arg: string)=>number[]
+
+console.log(myFunction.parameters());
+// => ZodTuple<[ZodString, ZodNumber]>
+
+myFunction.returnType();
+// => ZodBoolean
